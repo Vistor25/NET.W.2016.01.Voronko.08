1c9c4996c6f73a892324ab1acac0c843ceddf3fd
Виктор <victorvoronko@gmail.com>
2016-11-06 17:44:23 +0300

    add

---
+++ Task1/Customer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Customer:IFormattable
+    {
+        private string name;
+        private long contactPhone;
+        private decimal revenue;
+
+        public string Name
+        {
+            get;
+        }
+
+        public long ContactPhone
+        {
+            get;set;
+        }
+
+        public decimal Revenue
+        {
+            get;set;
+        }
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(string name, long contactPhone, decimal revenue)
+        {
+            Name = name;
+            ContactPhone = contactPhone;
+            Revenue = revenue;
+
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            switch (format) { 
+                case"NRC":
+                    return $"Customer record: {Name}, {Revenue.ToString("#,#.00", formatProvider)}, {ContactPhone:+# (###) ###-####}";
+                case "C":
+                    return $"Customer record: {ContactPhone}";
+                case "NR":
+                    return $"Customer record: {Name}, {Revenue.ToString("#,#.00", formatProvider)}";
+                case "N":
+                    return $"Customer record: {Name}";
+                case "R":
+                    return $"Customer record: {Revenue}";
+                default:
+                    return $"Customer record: {Name}, {Revenue.ToString("#,#.00", formatProvider)}, {ContactPhone:+# (###) ###-####}";
+
+            }
+        }
+    }
+}
---
+++ Task1/ExtendedCustomer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+   public static class ExtendedCustomer
+    {
+        public static string ExtendetToString(this Customer customer, string format, IFormatProvider formatProvider)
+        {
+            switch (format)
+            {
+                case "CRN":
+                    return $"Customer record:{customer.ContactPhone:+# (###) ###-####},{customer.Revenue.ToString("#,#.00", formatProvider)},{customer.Name}";
+                case "NRCByRow":
+                    return $"Customer record:\n{customer.Name},\n{customer.Revenue.ToString("#,#.00", formatProvider)},\n{customer.ContactPhone:+# (###) ###-####}";
+                default:
+                    return "error";
+            }
+        }
+    }
+}
